07252151cfb17bbf0f85ee516632bc526ddd20e3
Raghvendra <raghvendrasrivastava14@gmail.com>
2017-05-29 19:17:53 +0530

    added kill connection functionality

--- app/app.js
+++ app/app.js
@@ -24,25 +24,31 @@ io.on('connection', function(socket) {
 	socket.on('request', function(data) {
 		var timeSpent = 0;
 		var timeInterval = (data.timeout < 1000) ? data.timeout : 1000;
-		console.log('Got Data')
+		// console.log('Got Data')
 		clients.push({
+			status: 'progress',
 			connId: data.connId,
 			timeout: data.timeout,
 			timeSpent: 0
 		})
-		console.log(clients)
+		// console.log(clients)
 		var promise = new Promise(function (resolve, reject) {
 			var interval = setInterval(function() {
-				console.log('Time Interval:', timeInterval)
+				// console.log('Time Interval:', timeInterval)
 				timeSpent += timeInterval;
 				for (var i = 0; i < clients.length; i++) {
 					if (clients[i].connId === data.connId) {
-						if (clients[i].timeout !== timeSpent.toString()) {
+						if (clients[i].timeout !== timeSpent.toString() && clients[i].status === 'progress') {
 							clients[i].timeSpent = timeSpent
-							console.log('Updated timeSpent:', clients[i])
+							// console.log('Updated timeSpent:', clients[i])
+						} else if (clients[i].status === 'killed') {
+							// console.log('Process Killed')
+							clients.splice(i, 1)
+							clearInterval(interval)
+							reject('NoSuccess')
 						} else { // To remove the processed connection.
 							clients.splice(i, 1)
-							console.log('Deleting connection data', clients)
+							// console.log('Deleting connection data', clients)
 						}
 						break
 					}
@@ -60,12 +66,48 @@ io.on('connection', function(socket) {
 		});
 
 		promise.then(function(result) {
-			console.log('Interval over', data.connId)
-			console.log('After processing', clients)
+			// console.log('Interval over', data.connId)
+			// console.log('After processing', clients)
 			socket.emit('response', {status: "ok", connId: data.connId})	
-		}, function(err) {
-			console.log('Interval cancel')
+		}, function(result) {
+			// console.log('Interval cancel')
+			// console.log(clients)
+			socket.emit('response', {status: "killed", connId: data.connId})
 		})
+	})
+
+	socket.on('get-status', function() {
+		var result = ''
+		for (const obj of clients) {
+			result += '"' + obj.connId + '":"' + obj.timeSpent + '",' 
+		}
+		if (result.length) {
+			result = result.substring(0, result.length - 1)
+			// console.log(result)
+		}
+		socket.emit('status', {result})
+	})
+
+	socket.on('kill-connId', function(data) {
+		var isAvailable = false;
+		for (var i = 0; i < clients.length; i++) {
+			if (clients[i].connId === data.connId) {
+				isAvailable = true;
+				if (clients[i].status === 'progress') {
+					// console.log('Killable')
+					clients[i].status = 'killed'
+					// console.log(clients)
+					socket.emit('killed', {status: "ok", connId: data.connId})
+				} else {
+					console.log('Non Killable')
+				}
+				break
+			}
+		}
+		if (isAvailable === false) {
+			var dataPacket = "invalid connection Id: " + data.connId.toString()
+			socket.emit('killed', {status: dataPacket})
+		}
 	})
 
 	socket.on('disconnect', function() {
--- app/views/scripts/socket.js
+++ app/views/scripts/socket.js
@@ -1,5 +1,7 @@
 var socket = io();
 angular.module('DCRApp', []).controller('DCRForm', ['$scope', function ($scope) {
+
+	//On Click Functions
 	$scope.request = function() {
 		var data = {
 			connId: $scope.connId,
@@ -8,16 +10,67 @@ angular.module('DCRApp', []).controller('DCRForm', ['$scope', function ($scope) 
 		$scope.connId = ''
 		$scope.timeout = ''
 		$scope.insertResponse = ''
+		$scope.insertStatus = ''
+		$scope.insertKillStatus = ''
 		console.log('Sending request with', data)
 		socket.emit('request', data)
 	}
 
+	$scope.getStatus = function() {
+		$scope.insertResponse = ''
+		$scope.insertStatus = ''
+		$scope.insertKillStatus = ''
+		socket.emit('get-status')
+	}
+
+	$scope.deleteId = function() {
+		var data = {
+			connId: $scope.killConnId
+		}
+		$scope.killConnId = ''
+		$scope.insertResponse = ''
+		$scope.insertStatus = ''
+		$scope.insertKillStatus = ''
+		socket.emit('kill-connId', data)
+	}
+
+	// Socket functions
 	socket.on('response', function(data) {
 		console.log(data)
+		var result = ''
 		if (angular.equals(data.status, "ok")) {
-			$scope.$apply(function () {
-				$scope.insertResponse = 'Result: {"status": "ok", "connId": ' + data.connId + '}'
-			})
-		}	
+			result = 'Result: {"status": "ok", "connId": ' + data.connId + '}'
+		} else if (angular.equals(data.status, "killed")){
+			result = 'Result: {"status": "killed", "connId": ' + data.connId + '}'
+		}
+		$scope.$apply(function () {
+				$scope.insertResponse = result
+		})
+	})
+
+	socket.on('status', function(data) {
+		var status = ''
+		if (angular.equals(data.result, '')) {
+			status = 'No current connections found.'
+		} else {
+			console.log(data)
+			status = 'Result: {' + data.result + '}'
+		}
+		$scope.$apply(function() {
+			$scope.insertStatus = status
+		})
+	})
+
+	socket.on('killed', function(data) {
+		console.log(data)
+		var result = ''
+		if (angular.equals(data.status, "ok")) {
+			result = 'Result: {"status":"ok", "connId": ' + data.connId + '}'
+		} else {
+			result = 'Result: {"status":"' + data.status + '"}'
+		}
+		$scope.$apply(function() {
+			$scope.insertKillStatus = result
+		})
 	})
 }])
